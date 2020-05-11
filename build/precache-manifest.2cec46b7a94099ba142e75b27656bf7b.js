e0f8daf93fc043875ee1b5c283f553ae14c69cef
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-05-11 21:58:05 +0300

    Deploy

---
+++ build/precache-manifest.2cec46b7a94099ba142e75b27656bf7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab0f06e325d1967d4aaf03ffb6c2020",
+    "url": "/React-Simple-Snake/index.html"
+  },
+  {
+    "revision": "ae10be2531ec36d1aa78",
+    "url": "/React-Simple-Snake/static/css/main.7c6c6f3b.chunk.css"
+  },
+  {
+    "revision": "f1c5a4a6d3d98cffcf3d",
+    "url": "/React-Simple-Snake/static/js/2.533829e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Simple-Snake/static/js/2.533829e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae10be2531ec36d1aa78",
+    "url": "/React-Simple-Snake/static/js/main.69aae22e.chunk.js"
+  },
+  {
+    "revision": "f717031a40f0c3c1e2dd",
+    "url": "/React-Simple-Snake/static/js/runtime-main.ca19f719.js"
+  }
+]);
