f88a48ec977b4f29c632ce4679004bd2479bc5ef
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-05-11 21:50:11 +0300

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-### Simple snake game made with React
-
-### To run a program on local machine `npm start`
-
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ precache-manifest.b0741d5dd77a4097ffc8265ee5bca936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dd17082f5058c9508b1d112319c056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dfa4497e95cc89d7afe",
+    "url": "/static/css/main.7c6c6f3b.chunk.css"
+  },
+  {
+    "revision": "f1c5a4a6d3d98cffcf3d",
+    "url": "/static/js/2.533829e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.533829e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfa4497e95cc89d7afe",
+    "url": "/static/js/main.bd119b01.chunk.js"
+  },
+  {
+    "revision": "56bd907b9b98eea4565a",
+    "url": "/static/js/runtime-main.5b1fdad4.js"
+  }
+]);
